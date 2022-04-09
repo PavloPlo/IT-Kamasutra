154212c352a02e99fee63e551ef3f867b9543c33
Pavlo Plokhinov <plohinov.p.y@gmail.com>
2022-04-09 12:28:39 +0200

    header is decomponised!:)

--- my-great-site/src/App.js
+++ my-great-site/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Header from './Header/Header';
+import Header from './components/Header/Header';
 
-import Body from './Body/Body';
-import Footer from './Footer/Footer';
+import Body from './components/Body/Body';
+import Footer from './components/Footer/Footer';
 const App = () => {
   return (
     <div className="app-wrapper" >
